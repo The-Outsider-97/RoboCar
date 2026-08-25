712f0108c48e62b5bdd29b43a5b73f24aa110a5a
J.E. Remy <jeanerolle97@gmail.com>
2026-08-25 17:49:44 +0200

    Add _to_float and _to_int helper functions
    Added helper functions to convert values to float and int safely.

--- utils/rc_helpers.py
+++ utils/rc_helpers.py
@@ -7,9 +7,26 @@ import json
 
 from collections import deque
 from datetime import datetime, timedelta
-from typing import Any, Mapping #, TYPE_CHECKING, 
+from typing import Any, Mapping, Optional
 
 import numpy as np # type: ignore
 import pandas as pd # type: ignore
 
 from .rc_errors import *
+
+
+def _to_float(v) -> Optional[float]:
+    try:
+        if v is None:
+            return None
+        return float(v)
+    except Exception:
+        return None
+
+def _to_int(v) -> Optional[int]:
+    try:
+        if v is None:
+            return None
+        return int(v)
+    except Exception:
+        return None
