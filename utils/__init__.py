73e30d058952f4d586a26b69b7dcd7a1f554bc92
J.E. Remy <jeanerolle97@gmail.com>
2026-08-25 17:33:46 +0200

    Rename rc_error to rc_errors and update exports

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,15 +1,15 @@
-from .rc_error import *
+from .rc_errors import *
 from .rc_helpers import *
 from .config_loader import *
 
 
-from .rc_error import __all__ as _rc_error_exports
-from .rc_helpers import __all__ as _rc_helpers_exports
+from .rc_errors import __all__ as _rc_error_exports
+from .rc_helpers import __all__ as _rc_helper_exports
 from .config_loader import __all__ as _config_loader_exports
 
 
 __all__ = [
     *_rc_error_exports,
-    *_rc_helpers_exports,
+    *_rc_helper_exports,
     *_config_loader_exports,
 ] # type: ignore
